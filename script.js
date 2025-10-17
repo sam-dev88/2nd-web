7743e9818a40a5d06cf88c6d9e6a4d4c32b40ee0
Tayansam <samiraramo815@gmail.com>
2025-10-17 22:31:05 +0300

    updated index.html, style.css, js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
 
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  burger.classList.toggle("toggle");
 
-burger.addEventListener('click', () => {
-  nav.classList.toggle('active');
-  burger.classList.toggle('toggle');
+  if (nav.classList.contains("active")) {
+    document.body.style.overflow = "hidden"; // ✅ disable scroll
+  } else {
+    document.body.style.overflow = ""; // enable scroll
+  }
 });
-
