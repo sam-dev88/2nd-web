cdad100b7352a69e6995e5b01afb4c9e4183dafb
Tayansam <samiraramo815@gmail.com>
2025-10-17 20:08:34 +0300

    updated index.html and style.css and js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  burger.classList.toggle('toggle');
+});
+
